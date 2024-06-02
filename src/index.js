a85368115a029644023564aed8b911267e1d56f6
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-06-02 21:38:08 +0600

    feat: Add express server setup and connection to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running at port: ${process.env.PORT}`);
+        });
+        // app.on((err) => {
+        //     console.log("Server connect Error: ", err);
+        // });
+    })
+    .catch((err) => {
+        console.log("MongoDB Error: ", err);
+    });
